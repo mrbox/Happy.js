498d91fc25d0877ac189ee6cd714f82df77c5a07
Jakub Paczkowski <jakub@paczkowski.eu>
2011-11-02 11:06:12 +0100

    Allow using a 'tests' parameter for a many field validators

--- happy.js
+++ happy.js
@@ -8,6 +8,15 @@
     function getError(error) {
       return $('<span id="'+error.id+'" class="unhappyMessage">'+error.message+'</span>');
     }
+    function getErrorsList(errors, id){
+        var list = $('<ul id="' + id + '" class="unhappyMessage"></ul>');
+
+        $.each(errors, function(index,value){
+            $(list).append('<li>' + value.message + '</li>');
+        });
+
+        return list;
+    }
     function handleSubmit() {
       var errors = false, i, l;
       for (i = 0, l = fields.length; i < l; i += 1) {
@@ -29,25 +38,32 @@
     function isFunction (obj) {
       return !!(obj && obj.constructor && obj.call && obj.apply);
     }
+    function isArray(obj) {
+        return obj.constructor === Array;
+    }
     function processField(opts, selector) {
       var field = $(selector),
         error = {
           message: opts.message,
           id: selector.slice(1) + '_unhappy'
-        },
-        errorEl = $(error.id).length > 0 ? $(error.id) : getError(error);
+        };
+        var errorEl = $(error.id).length > 0 ? $(error.id) : getError(error);
+        var error_id = $(error.id);
         
       fields.push(field);
       field.testValid = function (submit) {
-        var val,
+        var val, gotFunc, temp,
           el = $(this),
-          gotFunc,
-          error = false,
-          temp, 
+          errors = [],
+          error_bool = false,
+          error_bool_in = false,
           required = !!el.get(0).attributes.getNamedItem('required') || opts.required,
           password = (field.attr('type') === 'password'),
           arg = isFunction(opts.arg) ? opts.arg() : opts.arg;
-        
+
+          
+        $(errorEl).remove();
+          errorEl = $(error.id).length > 0 ? $(error.id) : getError(error);
         // clean it or trim it
         if (isFunction(opts.clean)) {
           val = opts.clean(el.val());
@@ -56,7 +72,6 @@
         } else {
           val = el.val();
         }
-        
         // write it back to the field
         el.val(val);
         
@@ -65,20 +80,37 @@
         
         // check if we've got an error on our hands
         if (submit === true && required === true && val.length === 0) {
-          error = true;
+          error_bool = true;
         } else if (gotFunc) {
-          error = !opts.test(val, arg);
+          error_bool = !opts.test(val, arg);
         }
-        
-        if (error) {
+          
+        // if there are many test for a field
+        if (opts.tests !== undefined && isArray(opts.tests) && val.length > 0){
+            //console.log(val.length + " " + (this.id || $(this).attr('id')));
+            if (error_bool) errors.push(error); // list of errors will start with the 'main error'
+            
+            $.each(opts.tests, function(index, value){
+                gotFunc = ((val.length > 0 || required === 'sometimes') && isFunction(value.test));
+                if (gotFunc) {
+                    var temp_err = value.test(val, value.arg);
+                    if (!temp_err) {
+                        errors.push({
+                            message: value.message || ''
+                        });
+                        error_bool_in = true;
+                    }
+                }
+            });
+            if(error_bool_in){
+                errorEl = getErrorsList(errors, error.id);
+            }
+        }
+        console.log(errorEl);
+        if (error_bool) {
           el.addClass('unhappy').before(errorEl);
           return false;
         } else {
-          temp = errorEl.get(0);
-          // this is for zepto
-          if (temp.parentNode) {
-            temp.parentNode.removeChild(temp);
-          }
           el.removeClass('unhappy');
           return true;
         }
